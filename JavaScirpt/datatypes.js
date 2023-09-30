61297209af443ed05414c5cf3e8b1ea0a74ed81b
Sanket Naik <sanketnaik@Sankets-MacBook-Air.local>
2023-09-30 23:59:31 +0530

    trying

---
+++ JavaScirpt/datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all Js code as newer version
+
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3 +
+    3)
+//code readabilty should be high
+
+
+
+// DATA TYPES ----->
+
+
+let name = "Sanket"
+let age = 19;
+let isLoggedIn = false // or true;
+
+
+// number =>2 to power 53
+//bigint
+
+//string=" ";
+
+//boolean (true or false)
+
+// /null=>stand alone value
+//undefined=> value is not assigened
+//symbol
+
+
+//object
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
---
+++ JavaScirpt/variables.js
@@ -0,0 +1,21 @@
+const accountId = 144562
+let accountEmail = "SanketNaik@gmail.com"
+var accountPass = "12345"// var is not used nowadays
+accountCity = "Margao"
+
+accountEmail = "San@gmail.com"
+accountPass = "2828282"
+accountCity = "Canacona"
+let accountState;
+let accountBlock="hello123445@gmail.com"
+/*
+Note:-
+Prefer not to use Var Coz of Issue in Block scope and functional scope
+
+*/
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState,accountBlock]);
+
